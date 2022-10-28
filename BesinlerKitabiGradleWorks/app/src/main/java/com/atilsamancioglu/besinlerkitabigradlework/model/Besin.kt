c5a5d84ea9b2c6c519378aa9a13162eda30c72b3
Furkan Senol <frkn.snl@hotmail.com>
2022-10-28 17:23:17 +0300

    besin silindi

--- BesinlerKitabiGradleWorks/app/src/main/java/com/atilsamancioglu/besinlerkitabigradlework/model/Besin.kt
+++
@@ -1,9 +0,0 @@
-package com.atilsamancioglu.besinlerkitabigradlework.model
-
-data class Besin(
-    val besinIsim :String?,
-    val besinKalori:String?,
-    val besinKarbonhidrat:String?,
-    val besinProtein:String?,
-    val besinYag:String?,
-    val besinGorsel:String?) {}
